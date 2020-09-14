e66dab6cf52a42b272eeb896a2b6b04275429092
piiskatti <iisakki.kamula@gmail.com>
2020-09-14 11:07:17 +0300

    fucnction made for dropbox file handling via backhand api

--- src/main/java/endpoint_controllers/DropboxController.java
+++ src/main/java/endpoint_controllers/DropboxController.java
@@ -1,5 +1,6 @@
 package endpoint_controllers;
 
+import org.apache.http.HttpEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,16 +30,24 @@ public class DropboxController {
 	// Upload file to dropbox based on database name, table name, and row id
 	@PostMapping("/dropbox/uploadREST/{database_name}/{table_name}/{row_id}")
 	@ResponseBody
-	public void upload_file(@PathVariable("database_name") String database_name,
+	public String upload_file(@PathVariable("database_name") String database_name,
 			@PathVariable("table_name") String table_name, @PathVariable("row_id") int row_id,
 			HttpServletRequest request, HttpServletResponse response) {
 		String backend_url = PublicController.get_backend_domain(request) + "/dropbox/uploadREST/" + database_name + "/" + table_name + "/" + row_id;
 		try {
-			response.sendRedirect(backend_url);
-		} catch (IOException e) {
+			List<Part> parts = request.getParts().stream().collect(Collectors.toList());
+			for (Part file : parts) {
+				
+				HttpEntity responseEntity = PublicController.make_rest_call_POST_UPLOAD_DROPBOX_FILE(file, backend_url, file.getSubmittedFileName());
+				log("responseEntity: " + responseEntity);
+			}
+			return "{}";
+//			response.sendRedirect(backend_url);
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return null;
 	}
 
 
@@ -48,8 +57,8 @@ public class DropboxController {
 			@PathVariable("database_name") String database_name, @PathVariable("file_id") String file_id)
 			throws Exception {
 
-		String backend_url = PublicController.get_backend_domain(request) + "/dropbox/" + database_name + "/download/" + file_id;
-		PublicController.make_rest_call_GET(backend_url);
+		String backend_url = PublicController.get_backend_domain(request) + "/dropbox/" + database_name + "/download";
+		PublicController.make_rest_call_POST_DOWNLOAD_DROPBOX_FILE(response, backend_url,file_id);
 	}
 
 	// Delete file based on file id
--- src/main/java/endpoint_controllers/PublicController.java
+++ src/main/java/endpoint_controllers/PublicController.java
@@ -2,22 +2,38 @@ package endpoint_controllers;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -56,13 +72,103 @@ public class PublicController {
 
 	public static String get_backend_domain(HttpServletRequest request) {
 		// TODO Auto-generated method stub
-		if(request.getRequestURI().toLowerCase().contains("localhost")) {
+		if(request.getRequestURL().toString().contains("localhost")) {
 			return "http://localhost:8080";
 		}
 		return "https://yritysonline-backend.herokuapp.com/";
 	}
 
+	public static HttpEntity make_rest_call_POST_UPLOAD_DROPBOX_FILE(Part file, String backend_url, String filename) {
+		
+		try {
+			CloseableHttpClient httpClient = HttpClients.createDefault();
+			HttpPost uploadFile = new HttpPost(backend_url);
+			MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+
+			// This attaches the file to the POST:
+			FileInputStream fis = (FileInputStream) file.getInputStream();
+			builder.addBinaryBody(
+			    "file",
+			    fis,
+			    ContentType.APPLICATION_OCTET_STREAM,
+			    filename
+			);
+
+			HttpEntity multipart = builder.build();
+			uploadFile.setEntity(multipart);
+			CloseableHttpResponse response;
+			response = httpClient.execute(uploadFile);
+			HttpEntity responseEntity = response.getEntity();
+			return responseEntity;
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static void make_rest_call_POST_DOWNLOAD_DROPBOX_FILE(HttpServletResponse response, String backend_url, String filename) {
+		log("backend url: " + backend_url);
+		String output = null;
+		try {
+			URL url = new URL(backend_url);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Accept", "application/json");
+			conn.setDoOutput(true);
+			conn.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+
+			try {
+				DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+				wr.write(filename.getBytes("UTF8"));
+				wr.flush();
+				wr.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			if (conn.getResponseCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+			}
+			
+
+			//initialize response
+			response.setContentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
+			response.setHeader("Content-Disposition", "attachment; filename=\"" + URLEncoder.encode(filename, "UTF-8")+"\";");
+			response.setHeader("Content-Transfer-Encoding", "binary");
+			
+			PublicController.copy(conn.getInputStream(), response.getOutputStream());
+			
+			conn.disconnect();
+
+
+			response.getOutputStream().flush();
+			response.getOutputStream().close();
+
+		} catch (MalformedURLException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		}
+	}
+	
+	private static void copy(InputStream source, OutputStream target) throws IOException {
+	    byte[] buf = new byte[8192];
+	    int length;
+	    while ((length = source.read(buf)) > 0) {
+	        target.write(buf, 0, length);
+	    }
+	}
+
 	public static String make_rest_call_POST_JSON(String backend_url, String body_json) {
+		log("backend url: " + backend_url);
 		String output = null;
 		try {
 			URL url = new URL(backend_url);
